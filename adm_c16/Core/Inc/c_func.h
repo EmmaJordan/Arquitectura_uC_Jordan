00d05635a928e6e0f97557524a33de7efaca7c3e
EmmaJordan <ejjordan91@gmail.com>
2022-05-31 17:22:51 -0300

    modularizacion funciones en C

---
+++ adm_c16/Core/Inc/c_func.h
@@ -0,0 +1,13 @@
+/*
+ * c_func.h
+ *
+ *  Created on: 31 may. 2022
+ *      Author: Win10
+ */
+
+#ifndef INC_C_FUNC_H_
+#define INC_C_FUNC_H_
+
+
+
+#endif /* INC_C_FUNC_H_ */
---
+++ adm_c16/Core/Src/c_func.c
@@ -0,0 +1,8 @@
+/*
+ * c_func.c
+ *
+ *  Created on: 31 may. 2022
+ *      Author: Win10
+ */
+
+

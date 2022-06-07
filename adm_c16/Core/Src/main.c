3798fd1768a7089fb8ab8e84030cee8f096aa535
EmmaJordan <ejjordan91@gmail.com>
2022-06-07 12:56:11 -0300

    mejora ejercicio satura a 12bits sin usat

--- adm_c16/Core/Src/main.c
+++ adm_c16/Core/Src/main.c
@@ -173,6 +173,22 @@ void productoEscalar12(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitu
 		}
 	}
 }
+
+void invertir (uint16_t * vector, uint32_t longitud)
+{
+	uint16_t aux;
+	uint32_t index1 = 0, index2 = longitud - 1, loops;
+
+	loops = longitud/2;
+	while(loops--)
+	{
+		aux = vector[index2];
+		vector[index2] = vector[index1];
+		vector[index1] = aux;
+		index1++;
+		index2--;
+	}
+}
 /**
   * @brief  The application entry point.
   * @retval int

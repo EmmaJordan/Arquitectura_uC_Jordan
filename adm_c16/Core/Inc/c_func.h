172713bfd559b639803d623f1c5be31eda359de7
EmmaJordan <ejjordan91@gmail.com>
2022-06-07 13:01:56 -0300

    modularización c_func

--- adm_c16/Core/Inc/c_func.h
+++ adm_c16/Core/Inc/c_func.h
@@ -8,6 +8,20 @@
 #ifndef INC_C_FUNC_H_
 #define INC_C_FUNC_H_
 
+#include <stdint.h>
 
+void llenaArreglo32(uint32_t *vectorOut, uint32_t longitud);
+
+void llenaArreglo16(uint16_t *vectorOut, uint32_t longitud);
+
+void zeros(uint32_t *vector, uint32_t longitud);
+
+void productoEscalar32(uint32_t *vectorIn, uint32_t *vectorOut, uint32_t longitud, uint32_t escalar);
+
+void productoEscalar16(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud, uint16_t escalar);
+
+void productoEscalar12(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud, uint16_t escalar);
+
+void invertir (uint16_t * vector, uint32_t longitud);
 
 #endif /* INC_C_FUNC_H_ */
--- adm_c16/Core/Src/c_func.c
+++ adm_c16/Core/Src/c_func.c
@@ -4,5 +4,72 @@
  *  Created on: 31 may. 2022
  *      Author: Win10
  */
+#include "c_func.h"
 
+void llenaArreglo32(uint32_t *vectorOut, uint32_t longitud)
+{
+	while(longitud--)
+	{
+		vectorOut[longitud] = longitud;
+	}
+}
 
+void llenaArreglo16(uint16_t *vectorOut, uint32_t longitud)
+{
+	while(longitud--)
+	{
+		vectorOut[longitud] = longitud;
+	}
+}
+//Recorro el arreglo de atrás hacia adelante
+void zeros(uint32_t *vector, uint32_t longitud)
+{
+	while(longitud--)
+	{
+		vector[longitud] = 0;
+	}
+}
+
+void productoEscalar32(uint32_t *vectorIn, uint32_t *vectorOut, uint32_t longitud, uint32_t escalar)
+{
+	while(longitud--)
+	{
+		vectorOut[longitud] = vectorIn[longitud] * escalar;
+	}
+}
+
+void productoEscalar16(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud, uint16_t escalar)
+{
+	while(longitud--)
+	{
+		vectorOut[longitud] = vectorIn[longitud] * escalar;
+	}
+}
+
+void productoEscalar12(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud, uint16_t escalar)
+{
+	while(longitud--)
+	{
+		vectorOut[longitud] = vectorIn[longitud] * escalar;
+		if(vectorOut[longitud] > (uint16_t) 0x0FFF)
+		{
+			vectorOut[longitud] = (uint16_t) 0x0FFF;
+		}
+	}
+}
+
+void invertir (uint16_t * vector, uint32_t longitud)
+{
+	uint16_t aux;
+	uint32_t index1 = 0, index2 = longitud - 1, loops;
+
+	loops = longitud/2;
+	while(loops--)
+	{
+		aux = vector[index2];
+		vector[index2] = vector[index1];
+		vector[index1] = aux;
+		index1++;
+		index2--;
+	}
+}
